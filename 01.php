5e8c5351a0d6b4ae8c92dee4070acb80a1396bca
Cintia <cintiaa_utrera@outlook.es>
2023-07-13 16:29:43 -0300

    creando primera clase

---
+++ 01.php
@@ -0,0 +1,31 @@
+<?php
+declare (strict_types = 1);
+include 'includes/header.php';
+// Definir una clase
+
+class Producto{    
+    public $nombre; // atributos
+    public $precio;  // atributos
+    public $disponible; // atributos
+
+
+    // Constructor es una funcion que se va a llamar de inmediato una vez que se crea una nueva instancia
+    public function __construct(string $nombre, int $precio, bool $disponible)
+    {
+        $this->nombre = $nombre;  // this se refiere al objeto que estas creando
+        $this->precio = $precio; 
+        $this->disponible = $disponible; 
+    }
+}
+
+// Crear instancias agregando algunos valores
+$producto = new Producto('tablet', 200, true);
+
+
+echo "<pre>";
+var_dump($producto);
+echo "</pre>";
+
+
+
+include 'includes/footer.php';
