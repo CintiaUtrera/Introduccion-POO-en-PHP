cfd9979e93b60c677c6cf19403e4c1b44cb51dc1
Cintia <cintiaa_utrera@outlook.es>
2023-07-13 16:36:57 -0300

    creando atributos

--- 01.php
+++ 01.php
@@ -4,17 +4,17 @@ include 'includes/header.php';
 // Definir una clase
 
 class Producto{    
-    public $nombre; // atributos
-    public $precio;  // atributos
-    public $disponible; // atributos
+    //public $nombre; // atributos
+    //public $precio;  // atributos
+    //public $disponible; // atributos
 
 
     // Constructor es una funcion que se va a llamar de inmediato una vez que se crea una nueva instancia
-    public function __construct(string $nombre, int $precio, bool $disponible)
+    public function __construct(public string $nombre, public int $precio, public bool $disponible)
     {
-        $this->nombre = $nombre;  // this se refiere al objeto que estas creando
-        $this->precio = $precio; 
-        $this->disponible = $disponible; 
+        //$this->nombre = $nombre;  // this se refiere al objeto que estas creando
+        //$this->precio = $precio; 
+        //$this->disponible = $disponible; 
     }
 }
 
