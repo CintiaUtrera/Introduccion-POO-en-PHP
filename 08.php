c00736c237ead4ef6b9abdeb22c8e1e859da29d6
Cintia <cintiaa_utrera@outlook.es>
2023-07-24 20:33:30 -0300

    namespace

--- 08.php
+++ 08.php
@@ -1,17 +1,24 @@
 <?php include 'includes/header.php';
 
 // Incluir las otras clases
-//require "clases/Clientes.php";
-//require "clases/Detalles.php";
+//require 'clases/Clientes.php';
+//require 'clases/Detalles.php';
 
 
+//NAMESPACE : es como darle un subnombre a la clase
+// NO se  puede tener dos clases que se llamen iguales
+
+use App\Clientes;
+use App\Detalles;
 
 function mi_autoload($clase) { // autoload va a buscar las clases 
-    
-    require __DIR__ . '/clases/'  . $clase . '.php';
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/'  . $partes[1] . '.php';
 }
 
 spl_autoload_register('mi_autoload');
+
+
 
 $detalles = new Detalles();
 $clientes = new Clientes();
