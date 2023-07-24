230d7f4d705d520d4dc74bbb3ba37bdc9e2b0c56
Cintia <cintiaa_utrera@outlook.es>
2023-07-24 14:07:59 -0300

    autoload de classes

--- 08.php
+++ 08.php
@@ -1,4 +1,19 @@
 <?php include 'includes/header.php';
 
+// Incluir las otras clases
+//require "clases/Clientes.php";
+//require "clases/Detalles.php";
+
+
+
+function mi_autoload($clase) { // autoload va a buscar las clases 
+    
+    require __DIR__ . '/clases/'  . $clase . '.php';
+}
+
+spl_autoload_register('mi_autoload');
+
+$detalles = new Detalles();
+$clientes = new Clientes();
 
 include 'includes/footer.php';
