567d87dd1fe680fecd7d2297eb54737e73dd305d
Cintia <cintiaa_utrera@outlook.es>
2023-08-02 11:28:26 -0300

    consultar la BD con POO y Mysqli

--- 09.php
+++ 09.php
@@ -1,4 +1,18 @@
 <?php include 'includes/header.php';
 
+// Crear nueva instancia para conectar a la BD con msqli
+$db = new mysqli('localhost', 'root', 'root', 'bienesraices_crud');
+
+
+$query = "SELECT titulo from propiedades";
+$stmt = $db->prepare($query);   // prepare es una sentencia preparada
+
+$stmt->execute(); //ejecutamos
+
+$stmt->bind_result($titulo); // retorna una variable llamada titulo
+
+$stmt->fetch(); // asignamos el resultado
+
+var_dump($titulo); // imprime el resultado
 
 include 'includes/footer.php';
